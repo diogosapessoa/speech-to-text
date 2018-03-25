faefb66a7007f131b87239b38414f704c96138e8
diogo leao sa pessoa <diogosapessoa@gmail.com>
2018-03-24 21:48:51 -0300

    Add project files.

---
+++ SpeechToText/Src/Activities/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Speech;
+using Android.Util;
+using Android.Widget;
+
+using SpeechToText.Src.Listeners;
+
+namespace SpeechToText.Src.Activities
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        SpeechRecognizer Recognizer { get; set; }
+        Intent SpeechIntent { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            var BtnStartSpeech = FindViewById<Button>(Resource.Id.btn_start_speech);
+            BtnStartSpeech.Click += BtnStartSpeech_Click;
+
+            var recListener = new RecognitionListener();
+            recListener.BeginSpeech += RecListener_BeginSpeech;
+            recListener.EndSpeech += RecListener_EndSpeech;
+            recListener.Error += RecListener_Error;
+            recListener.Ready += RecListener_Ready;
+            recListener.Recognized += RecListener_Recognized;
+
+            Recognizer = SpeechRecognizer.CreateSpeechRecognizer(this);
+            Recognizer.SetRecognitionListener(recListener);
+
+            SpeechIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
+            SpeechIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+            SpeechIntent.PutExtra(RecognizerIntent.ExtraCallingPackage, PackageName);
+        }
+
+        private void BtnStartSpeech_Click(object sender, System.EventArgs e) => Recognizer.StartListening(SpeechIntent);
+
+        private void RecListener_Ready(object sender, Bundle e) => Log.Debug(nameof(MainActivity), nameof(RecListener_Ready));
+
+        private void RecListener_BeginSpeech() => Log.Debug(nameof(MainActivity), nameof(RecListener_BeginSpeech));
+
+        private void RecListener_EndSpeech() => Log.Debug(nameof(MainActivity), nameof(RecListener_EndSpeech));
+
+        private void RecListener_Error(object sender, SpeechRecognizerError e) => Log.Debug(nameof(MainActivity), $"{nameof(RecListener_Error)}={e.ToString()}");
+
+        private void RecListener_Recognized(object sender, string recognized) => Toast.MakeText(this, recognized, ToastLength.Long).Show();
+    }
+}
---
+++ SpeechToText/Src/Listeners/RecognitionListener.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.OS;
+using Android.Runtime;
+using Android.Speech;
+
+namespace SpeechToText.Src.Listeners
+{
+    public class RecognitionListener : Java.Lang.Object, IRecognitionListener
+    {
+        public event EventHandler<Bundle> Ready;
+        public event Action BeginSpeech;
+        public event Action EndSpeech;
+        public event EventHandler<string> Recognized;
+        public event EventHandler<SpeechRecognizerError> Error;
+
+        public RecognitionListener() : base() { }
+
+        public void OnReadyForSpeech(Bundle @params) => Ready?.Invoke(this, @params);
+
+        public void OnBeginningOfSpeech() => BeginSpeech?.Invoke();
+
+        public void OnEndOfSpeech() => EndSpeech?.Invoke();
+
+        public void OnResults(Bundle results)
+        {
+            var matches = results.GetStringArrayList(SpeechRecognizer.ResultsRecognition);
+            if (matches != null && matches.Count > 0)
+            {
+                Recognized?.Invoke(this, matches[0]);
+            }
+        }
+
+        public void OnError([GeneratedEnum] SpeechRecognizerError error) => Error?.Invoke(this, error);
+
+        public void OnBufferReceived(byte[] buffer) { }
+
+        public void OnEvent(int eventType, Bundle @params) { }
+
+        public void OnPartialResults(Bundle partialResults) { }
+
+        public void OnRmsChanged(float rmsdB) { }
+    }
+}
